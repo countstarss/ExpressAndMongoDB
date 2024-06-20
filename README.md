fbd0cff8eab0a45043485542f0aee2f8fc1d03f6
lukeking <countstarss@163.com>
2024-06-20 20:06:42 +0800

    commit 1

---
+++ README.md
@@ -0,0 +1,23 @@
+# 启动项目
+    npm run dev    // "nodemon app.js"
+    npm start      // "run app.js"
+``` json
+"bcrypt": "^5.1.1",
+"connect-mongo": "^5.1.0",
+"cookie-parser": "^1.4.6",
+"dotenv": "^16.4.5",
+"ejs": "^3.1.10",
+"express": "^4.19.2",
+"express-ejs-layouts": "^2.5.1",
+"express-session": "^1.18.0",
+"jsonwebtoken": "^9.0.2",
+"method-override": "^3.0.0",
+"mongoose": "^8.4.3",
+"nodemon": "^3.1.4",
+"yarn": "^1.22.22"
+```
+
+## 共10小节
+
+## 第一部分
+
---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express();
+const PORT = 9000 || process.env.PORT;
+
+app.use(express.static('public'));
+
+// 添加模板引擎
+app.use(expressLayouts);
+app.set('layout','layout/main');
+app.set('view engine','ejs')
+
+// 导入路由
+app.use('/',require('./server/routes/main'));
+
+app.listen(PORT,() => {
+    console.log(`app listening on port ${PORT}`);
+});
