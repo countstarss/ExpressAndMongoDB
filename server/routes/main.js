1051af0ffe3a1926d61b43b9d04b06f0f4e11c1b
lukeking <countstarss@163.com>
2024-06-21 21:42:45 +0800

    CleanUp Code

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Post = require('../models/posts');
 const { render } = require('ejs');
 
-
+// Home路由
 router.get('', async (req, res) => {
     try {
       const locals = {
@@ -43,48 +43,47 @@ router.get('', async (req, res) => {
   
   });
 
-// router.get('/blog', async (req, res) => {
-// try {  
-//     let perPage = 7;
-//     let page = req.query.page || 1;
-//     // 分页设计：每页显示5条，根据创建时间排序，显示最新的5条
-//     // 如果后边还有,那么就显示上一页的链接，到第二页，依次增加
+// blog路由
+router.get('/blog', async (req, res) => {
+try {  
+    let perPage = 7;
+    let page = req.query.page || 1;
+    // 分页设计：每页显示5条，根据创建时间排序，显示最新的5条
+    // 如果后边还有,那么就显示上一页的链接，到第二页，依次增加
 
-//     // 最前面一页的数据
-//     const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
-//     .skip(perPage * page - perPage)
-//     .limit(perPage)
-//     .exec();
+    // 最前面一页的数据
+    const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
 
-//     // Count is deprecated - please use countDocuments
-//     // const count = await Post.count();
-//     const count = await Post.countDocuments({});
-//     const nextPage = parseInt(page) + 1;
-//     const hasNextPage = nextPage <= Math.ceil(count / perPage);
-//     // 最后一页一定是小于或者等于perPage，之所以用ceil是因为，就算多出一个，也要多增加一页
-//     res.render('blog', { 
-//     data,
-//     current: page,
-//     nextPage: hasNextPage ? nextPage : null,
-//     // nextPage只是一个页码，实际上提供数据的一直是data，根据page不同，提供不同的数据
-//     currentRoute: '/blog'
-//     });
+    // Count is deprecated - please use countDocuments
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+    // 最后一页一定是小于或者等于perPage，之所以用ceil是因为，就算多出一个，也要多增加一页
+    res.render('blog', { 
+    data,
+    current: page,
+    nextPage: hasNextPage ? nextPage : null,
+    // nextPage只是一个页码，实际上提供数据的一直是data，根据page不同，提供不同的数据
+    currentRoute: '/blog'
+    });
 
-// } catch (error) {
-//     console.log(error);
-// }
+} catch (error) {
+    console.log(error);
+}
+});
 
-// });
 
-
-// no pagination version
+// No pagination version
 // router.get('',async (req,res) => {
 //     const locals = {
 //         title: "Express Blog",
 //         description : "Simple blog create with NodeJs, express & MongoDB",
 //         joke: "you are beautiful"
 //     }
-
 //     try{
 //         const data = await Post.find();
 //         res.render('home',{ locals,data });
@@ -94,8 +93,6 @@ router.get('', async (req, res) => {
 
 //     res.render('home',{ locals });
 // });
-
-
 
 
 // 插入数据
@@ -116,6 +113,7 @@ router.get('', async (req, res) => {
 //     ])
 // };
 // insertPostData();
+
 
 // function insertPostData () {
 //   Post.insertMany([
@@ -161,17 +159,15 @@ router.get('', async (req, res) => {
 //     },
 //   ])
 // }
-
 // insertPostData();
 
 
-
-
-
+// About路由
 router.get('/about',(req,res) => {
     res.render('about');
 })
 
+// Contact
 router.get('/contact',(req,res) => {
     res.render('contact');
 })
